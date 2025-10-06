54e4e0e5b604a061652a883343d6e5f3d4467d0c
Featherless-bipod <featherlessbipod@outlook.com>
2025-10-06 12:23:13 -0400

    normal files

---
+++ mlpModel.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+import numpy as np
+import pandas as pd
+
+class MLP(nn.Module):
+    def __init__(self, input_size, num_classes, hidden_size1=128, hidden_size2=64):
+        super(MLP, self).__init__()
+        self.layers = nn.Sequential(
+            nn.Linear(input_size, hidden_size1),
+            nn.ReLU(),  # Activation function
+            nn.Dropout(0.3), # Dropout for regularization
+            nn.Linear(hidden_size1, hidden_size2),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(hidden_size2, num_classes) # Output layer
+        )
+
+    def forward(self, x):
+        return self.layers(x)
