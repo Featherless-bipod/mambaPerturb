3a1def7c9b70ded338cc0867cc0123b793dfb001
Featherless-bipod <featherlessbipod@outlook.com>
2025-10-07 08:26:40 -0400

    +optuna

--- mlpModel.py
+++ mlpModel.py
@@ -4,16 +4,22 @@ import numpy as np
 import pandas as pd
 
 class MLP(nn.Module):
-    def __init__(self, input_size, num_classes, hidden_size1=128, hidden_size2=64):
+    def __init__(self, input_size, num_classes, hidden_size1=219, hidden_size2=330,hidden_size3 = 219, hidden_size4 = 128):
         super(MLP, self).__init__()
         self.layers = nn.Sequential(
             nn.Linear(input_size, hidden_size1),
             nn.ReLU(),  # Activation function
             nn.Dropout(0.3), # Dropout for regularization
             nn.Linear(hidden_size1, hidden_size2),
-            nn.ReLU(),
-            nn.Dropout(0.3),
-            nn.Linear(hidden_size2, num_classes) # Output layer
+            nn.ReLU(),  # Activation function
+            nn.Dropout(0.3), # Dropout for regularization
+            nn.Linear(hidden_size2, hidden_size3),
+            nn.ReLU(),  # Activation function
+            nn.Dropout(0.3), # Dropout for regularization)
+            nn.Linear(hidden_size3, hidden_size4),
+            nn.ReLU(),  # Activation function
+            nn.Dropout(0.3), # Dropout for regularization)
+            nn.Linear(hidden_size4, num_classes) # Output layer
         )
 
     def forward(self, x):
